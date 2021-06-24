6687a34c1ff5c2b4565bc8764bb8b81e29c358d1
lagelisu <wwdddwwok@126.com>
2021-06-24 16:59:30 +0800

    增加文件

---
+++ test.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-24 16:55:56
+ * @LastEditTime: 2021-06-24 16:56:07
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \test\test.js
+ */
+console.log('测试代码');
